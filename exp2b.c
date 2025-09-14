e678165ae63149099b7fb75f3b0ab0ed449c38de
HIMANSHU7739 <himanshu30506@gmail.com>
2025-09-14 20:15:30 +0530

    Add files via upload

---
+++ exp2b.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int temperature;
+    printf("enter the temp. in celsius :");
+    scanf("%d",&temperature);
+    float fahrenhit;
+    fahrenhit=(temperature*9/5)+32;
+    printf("temperature in fahrenhit is :%f",fahrenhit);
+    return 0;
+}
